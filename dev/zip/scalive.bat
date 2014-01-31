78c851af89711faf493d95445156097d3ce30f0c
Ngoc Dao <ngocdaothanh@gmail.com>
2014-01-31 16:20:47 +0900

    Fix CLASS_PATH

--- dev/zip/scalive.bat
+++ dev/zip/scalive.bat
@@ -4,4 +4,6 @@ set JAVA_OPTS=-Djava.awt.headless=true
 set ROOT_DIR=%~dp0
 cd "%$ROOT_DIR%"
 
-java %JAVA_OPTS% -cp %ROOT_DIR% scalive.AgentLoader %$ROOT_DIR% %*
+set CLASS_PATH="%ROOT_DIR%\*;."
+
+java %JAVA_OPTS% -cp %CLASS_PATH% scalive.AgentLoader %$ROOT_DIR% %*
